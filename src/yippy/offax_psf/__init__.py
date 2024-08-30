cdf559b236b35de042a2b92280abf0cbc3e7dfcc
Corey Spohn <cas584@cornell.edu>
2024-08-29 22:01:05 -0400

    chore: Clean up old offax_psf classes

--- src/yippy/offax_psf/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Init file for offax_psf modules."""
-
-__all__ = ["OneD", "TwoD", "QuarterSymmetric"]
-
-from .one_d import OneD
-from .quarter_symmetric import QuarterSymmetric
-from .two_d import TwoD
